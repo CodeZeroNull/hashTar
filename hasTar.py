975e60710e5c0f6c4f1f484d40e0adb5b17a9079
CodeZeroNull <code0@runbox.com>
2024-09-23 14:33:30 +0200

    changed to function and removed harcoded input and output

--- hasTar.py
+++ hasTar.py
@@ -1,15 +1,26 @@
 import tarfile
 import hashlib
 
+"""
+This script calculates hashes for every file inside a tarfile and creates a checksum file
+Usage
+
+"""
+
+
 def checksum(file_to_hash):
     hashresult = hashlib.sha1()
     for chunk in iter(lambda: file_to_hash.read(4096), b''):
         hashresult.update(chunk)
     return hashresult.hexdigest()
 
-with tarfile.open('./test.tar') as tar_input:
-    with open('test.tar.sha1', 'w') as checksums_file:
-        for member in tar_input.getmembers():
-            if member.isreg():  # skip if not file (folders are members, hashing them fails)
-                with tar_input.extractfile(member) as _file:
-                    checksums_file.write('{}  ./{}\n'.format(checksum(_file), member.name))
+def hashtar(input_tar_file):
+    with tarfile.open(input_tar_file) as tar_input:
+        outputname = input_tar_file + 'sha1'
+        with open(outputname, 'w') as checksums_file:
+            for member in tar_input.getmembers():
+                if member.isreg():  # skip if not file (folders are members, hashing them fails)
+                    with tar_input.extractfile(member) as _file:
+                        checksums_file.write('{}  ./{}\n'.format(checksum(_file), member.name))
+
+
